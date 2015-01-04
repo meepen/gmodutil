52182a1683a3747479d26985e2e1f8dd32b7037a
MeepDarknessMeep <meepdarknessmeep@yahoo.com>
2015-01-04 14:49:03 -0500

    Make draw.GlowingText work with unicode

--- draw.lua
+++ draw.lua
@@ -10,53 +10,53 @@ local fontdata = {
 local header = "glow_text_";
 surface.madefonts = surface.madefonts or {};
 local made = surface.madefonts;
-local wtable = {};
 
 function draw.GlowingText(text, font, x, y, col, colglow, colglow2)
 	local bfont1 = header..font;
 	local bfont2 = header..font.."2";
 	fontdata.font = font;
 	surface.SetFont(font);
-	wtable[1] = 0;
-	for i = 1, #text do
-		wtable[i + 1] = surface.GetTextSize(text[i]) + wtable[i];
-	end
-	wtable[1] = -1; -- fixes glitch
 	if(not made[font]) then
 		local _, h = surface.GetTextSize("A");
 		fontdata.blursize = 2;
-		fontdata.size = h + 2;
+		fontdata.size = h;
 		surface.CreateFont(bfont1, fontdata);
 		made[font] = true;
 		fontdata.blursize = 4;
-		fontdata.size = h + 4;
+		fontdata.size = h;
 		surface.CreateFont(bfont2, fontdata);
 	end
 	surface.SetTextPos(x,y);
 	surface.SetTextColor(col);
 	surface.DrawText(text);
-	for i = 1, #text do
-		local cw = table.remove(wtable, 1);
 		surface.SetFont(bfont1);
 		surface.SetTextColor(colglow or ColorAlpha(col,150));
-		surface.SetTextPos(x + cw, y);
-		surface.DrawText(text[i]);
+		surface.SetTextPos(x, y);
+		surface.DrawText(text);
 		surface.SetFont(bfont2);
 		surface.SetTextColor(colglow2 or colglow and ColorAlpha(colglow,50) or ColorAlpha(col, 50));
-		surface.SetTextPos(x + cw, y - 1);
-		surface.DrawText(text[i]);
-	end
-	table.remove(wtable,1);
+		surface.SetTextPos(x, y);
+		surface.DrawText(text);
 end
 
 --[[-----------
 	Example
 -----------]]--
 --[[
-local col = Color(255,255,0,255);
-local colglow = Color(255,255,0,100);
+local col = Color(255,0, 255,255);
+local colglow = ColorAlpha(col,0);
+local colglow2 = ColorAlpha(col,0);
+local intensity, intensity2 = 120, 60;
+local min, min2 = 40, 20
+local speed = 3;
 
 hook.Add("DrawOverlay", "", function()
-	draw.GlowingText("Gey!", "DermaDefault", 5, 5, col);
+	local offx = ScrW() / 2;
+	local offy = ScrH() / 2;
+	colglow.a = (math.sin(SysTime() * speed) / 2 + 0.5) * intensity + min;
+	colglow2.a = (math.sin(SysTime() * speed) / 2 + 0.5) * intensity2 + min2;
+	surface.SetDrawColor(0,0,0,255);
+	surface.DrawRect(offx,offy,300, 30);
+	draw.GlowingText("This is glowoowowow", "DermaDefault", 5+offx, 5+offy, col, colglow, colglow2);
 end);
 ]]
